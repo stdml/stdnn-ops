66a0db594c804cfa189073d7a1e9d3ee97c3c4a8
lg <lg4869@outlook.com>
2018-11-04 15:44:15 +0800

    padding support for pool

--- tests/test_pool.cpp
+++ tests/test_pool.cpp
@@ -1,7 +1,6 @@
 #include "testing.hpp"
 
-#include <nn/ops>
-#include <stdtensor>
+#include <nn/bits/ops/pool.hpp>
 
 TEST(pool_test, test_hw)
 {
@@ -117,6 +116,32 @@ TEST(pool_test, test_4d)
                                           max_pool_nchw::stride(2, 2));
             const auto y = ttl::tensor<float, 4>(op(x.shape()));
             ASSERT_EQ(y.shape(), ttl::internal::basic_shape<4>(n, c, 63, 127));
+            op(ref(y), view(x));
+        }
+    }
+}
+
+TEST(pool_test, test_padding)
+{
+    const uint32_t n = 7;
+    const uint32_t h = 128;
+    const uint32_t w = 256;
+    const uint32_t c = 3;
+
+    {
+        using pool = nn::ops::pool<nn::ops::pool_max, nn::ops::nhwc>;
+        const auto x = ttl::tensor<float, 4>(n, h, w, c);
+        {
+            const auto op = pool(pool::ksize(3, 3), pool::padding(2, 1));
+            const auto y = ttl::tensor<float, 4>(op(x.shape()));
+            ASSERT_EQ(y.shape(), ttl::internal::basic_shape<4>(n, 44, 86, c));
+            op(ref(y), view(x));
+        }
+        {
+            const auto op = pool(pool::ksize(3, 3), pool::padding(1, 1),
+                                 pool::stride(1, 1));
+            const auto y = ttl::tensor<float, 4>(op(x.shape()));
+            ASSERT_EQ(y.shape(), ttl::internal::basic_shape<4>(n, 128, 256, c));
             op(ref(y), view(x));
         }
     }
