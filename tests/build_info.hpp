cd1e840183ed6349864df5e6301c5e6db2c152cd
lg <lg4869@outlook.com>
2018-11-10 22:17:30 +0800

    have_fast_math

--- tests/build_info.hpp
+++ tests/build_info.hpp
@@ -55,7 +55,16 @@ constexpr const char *openblas_version =
 #endif
     ;
 
+constexpr const bool have_fast_math =
+#ifdef __FAST_MATH__
+    true
+#else
+    false
+#endif
+    ;
 }  // namespace _build_info
+
+inline std::string show_bool(bool b) { return b ? "true" : "false"; }
 
 struct build_info_s {
     static const bool is_linux = _build_info::is_linux;
@@ -65,29 +74,27 @@ struct build_info_s {
     static const bool is_gcc = _build_info::is_gcc;
 
     static const bool have_openblas = _build_info::have_openblas;
-
     const std::string openblas_version = _build_info::openblas_version;
-};
 
-inline std::string show_bool(bool b) { return b ? "true" : "false"; }
+    static const bool have_fast_math = _build_info::have_fast_math;
 
-inline void show_build_info()
-{
-    constexpr const char *tab = "    ";
-    build_info_s info;
-    std::stringstream ss;
-    ss << "build info:\n"
-       << "{\n";
-    ss << tab << "os: " << (info.is_mac ? "mac" : "linux") << "\n";
-    ss << tab << "is_clang: " << show_bool(info.is_clang) << "\n";
-    ss << tab << "is_gcc: " << show_bool(info.is_gcc) << "\n";
-    ss << tab << "have_openblas: " << show_bool(info.have_openblas) << "\n";
-    if (info.have_openblas) {
-        ss << tab << "openblas version: " << info.openblas_version << "\n";
+    std::string to_string()
+    {
+        constexpr const char *tab = "    ";
+        std::stringstream ss;
+        ss << "build info:\n"
+           << "{\n";
+        ss << tab << "os: " << (is_mac ? "mac" : "linux") << "\n";
+        ss << tab << "compiler: " << (is_clang ? "clang" : "gcc") << "\n";
+        ss << tab << "have_openblas: " << show_bool(have_openblas) << "\n";
+        if (have_openblas) {
+            ss << tab << "openblas version: " << openblas_version << "\n";
+        }
+        ss << tab << "have_fast_math: " << show_bool(have_fast_math) << "\n";
+        ss << "}\n";
+        return ss.str();
     }
-    ss << "}\n";
-    printf("%s\n", ss.str().c_str());
-}
+};
 
 template <typename F> class defer_t
 {
@@ -98,4 +105,7 @@ template <typename F> class defer_t
     ~defer_t() { f_(); }
 };
 
-defer_t _show_build_info([] { show_build_info(); });
+defer_t _show_build_info([] {
+    build_info_s info;
+    printf("%s\n", info.to_string().c_str());
+});
