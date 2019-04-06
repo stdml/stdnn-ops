b83b0484e830d4770501f117b04d57b0aec33fbd
lg <lg4869@outlook.com>
2019-04-06 22:29:51 +0800

    padding policy for pool layer

--- tests/test_layer_constructors.cpp
+++ tests/test_layer_constructors.cpp
@@ -45,3 +45,45 @@ void test_conv2d_layer_all()
     test_conv2d_layer<nn::ops::nhwc, nn::ops::rscd>(p);
     test_conv2d_layer<nn::ops::nchw, nn::ops::dcrs>(p);
 }
+
+struct pool2d_layer_params_t {
+    using dim_t = uint32_t;
+
+    dim_t ksize_h;
+    dim_t ksize_w;
+    dim_t stride_h;
+    dim_t stride_w;
+    dim_t pad_h_left;
+    dim_t pad_h_right;
+    dim_t pad_w_left;
+    dim_t pad_w_right;
+};
+
+template <typename pool_algo, typename image_order>
+void test_conv2d_layer(const pool2d_layer_params_t &p)
+{
+    using pool2d = nn::layers::pool<pool_algo, image_order>;
+    {
+        pool2d l1;
+        pool2d l2(pool2d::ksize(2, 2));
+    }
+    {
+        pool2d l1(ksize<pool2d>(p));
+        pool2d l2(ksize<pool2d>(p), pool2d::padding_same());
+        pool2d l3(ksize<pool2d>(p), pool2d::padding_same(), stride<pool2d>(p));
+        // FIXME: support fixed padding 2d
+    }
+}
+
+template <class pool_method> void _test_pool2d_all_bind_1()
+{
+    pool2d_layer_params_t p;
+    test_conv2d_layer<pool_method, nn::ops::nhwc>(p);
+    test_conv2d_layer<pool_method, nn::ops::nchw>(p);
+}
+
+void test_pool2d_layer_all()
+{
+    _test_pool2d_all_bind_1<nn::ops::pool_max>();
+    _test_pool2d_all_bind_1<nn::ops::pool_mean>();
+}
