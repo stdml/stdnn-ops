4fbe12b03464103e1e0f9b1b7d0bcbd51babb9dc
lg <lg4869@outlook.com>
2020-02-05 19:05:11 +0000

    io kernel

---
+++ include/ttl/nn/bits/kernels/io.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace ttl
+{
+namespace nn
+{
+namespace kernels
+{
+template <typename D, typename R>
+class readfile;
+
+template <typename D, typename R>
+class writefile;
+}  // namespace kernels
+}  // namespace nn
+}  // namespace ttl
