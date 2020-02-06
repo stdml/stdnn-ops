42393e85a33193f866c3ef1982b929feec0a48dd
lg <lg4869@outlook.com>
2020-02-06 13:14:12 +0000

    normalizer

--- include/ttl/nn/bits/kernels/cpu/normalization.hpp
+++ include/ttl/nn/bits/kernels/cpu/normalization.hpp
@@ -7,6 +7,27 @@
 namespace ttl::nn::kernels
 {
 template <typename R>
+class normalizer_mean_std
+{
+    static constexpr R eps = .000001f;
+
+  public:
+    R operator()(R x, R mean, R std) const { return (x - mean) / (std + eps); }
+};
+
+template <typename R>
+class normalizer_mean_var
+{
+    static constexpr R eps = .000001f;
+
+  public:
+    R operator()(R x, R mean, R var) const
+    {
+        return (x - mean) / (std::sqrt(var) + eps);
+    }
+};
+
+template <typename R>
 class batch_norm<host_memory, traits::nhwc, R>
 {
   public:
@@ -14,14 +35,15 @@ class batch_norm<host_memory, traits::nhwc, R>
                     const tensor_view<R, 1> &rolling_mean,
                     const tensor_view<R, 1> &rolling_var) const
     {
-        constexpr R eps = .000001f;
+        normalizer_mean_var<R> normalize;
+        // FIXME: use apply_bias<traits::nhwc, normalizer_mean_var>
         for (const auto l : range<0>(x)) {
             for (const auto i : range<1>(x)) {
                 for (const auto j : range<2>(x)) {
                     for (const auto k : range<3>(x)) {
                         y.at(l, i, j, k) =
-                            (x.at(l, i, j, k) - rolling_mean.at(k)) /
-                            (std::sqrt(rolling_var.at(k)) + eps);
+                            normalize(x.at(l, i, j, k), rolling_mean.at(k),
+                                      rolling_var.at(k));
                     }
                 }
             }
@@ -37,14 +59,14 @@ class batch_norm<host_memory, traits::nchw, R>
                     const tensor_view<R, 1> &rolling_mean,
                     const tensor_view<R, 1> &rolling_var) const
     {
-        constexpr R eps = .000001f;
+        normalizer_mean_var<R> normalize;
         for (const auto l : range<0>(x)) {
             for (const auto k : range<1>(x)) {
                 for (const auto i : range<2>(x)) {
                     for (const auto j : range<3>(x)) {
                         y.at(l, k, i, j) =
-                            (x.at(l, k, i, j) - rolling_mean.at(k)) /
-                            (std::sqrt(rolling_var.at(k)) + eps);
+                            normalize(x.at(l, k, i, j), rolling_mean.at(k),
+                                      rolling_var.at(k));
                     }
                 }
             }
