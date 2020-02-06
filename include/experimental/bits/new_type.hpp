3f338dae4432f372dd2a72b160aef6eb62ba6b4a
lg <lg4869@outlook.com>
2020-02-06 15:17:34 +0000

    make it nvcc compatiable

--- include/experimental/bits/new_type.hpp
+++ include/experimental/bits/new_type.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
-namespace std::experimental
+namespace std
 {
-
+namespace experimental
+{
 /*! new_type implements the newtype keyword from Haskell */
-template <typename T, typename K> class new_type : public T
+template <typename T, typename K>
+class new_type : public T
 {
     using T::T;
 };
-
-}  // namespace std::experimental
+}  // namespace experimental
+}  // namespace std
