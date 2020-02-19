bdf6934c540bf7cef4d2f1bf7ef11cf94964f808
lg <lg4869@outlook.com>
2020-02-19 22:53:56 +0000

    init im2col_idx_map

--- include/ttl/nn/bits/kernels/cpu/im2col.hpp
+++ include/ttl/nn/bits/kernels/cpu/im2col.hpp
@@ -7,6 +7,48 @@
 
 namespace ttl::nn::kernels
 {
+template <typename image_order, typename col_order>
+class im2col_idx_map;
+
+template <>
+class im2col_idx_map<traits::hwc, traits::hwrsc>
+    : public traits::im2col_trait<traits::hw>
+{
+    using im2col_trait::im2col_trait;
+
+  public:
+    im2col_idx_map(const im2col_trait &trait) : im2col_trait(trait) {}
+
+    template <typename N, typename Dim>
+    void operator()(const tensor_ref<N, 5> &y,
+                    const ttl::internal::basic_shape<3, Dim> &x) const
+    {
+        static_assert(std::is_signed<N>::value, "");
+        const auto [h, w, c] = x.dims();
+        // const auto [h_, w_, r, s, _c] = y.dims();
+        const auto &[h_sample, w_sample] = samples_;
+        const auto shape = y.shape();
+        for (auto idx : range(y.size())) {
+            const auto [i_, j_, u, v, k] = shape.expand(idx);
+            const auto i = h_sample(i_, u);
+            const auto j = w_sample(j_, v);
+            if (h_sample.inside(i, h) && w_sample.inside(j, w)) {
+                y.data()[idx] =
+                    x.offset(h_sample.unpad(i), w_sample.unpad(j), k);
+            } else {
+                y.data()[idx] = -1;
+            }
+        }
+    }
+
+    template <typename R, typename N>
+    void operator()(const tensor_ref<R, 5> &z, const tensor_view<N, 5> &y,
+                    const tensor_view<R, 3> &x) const
+    {
+        for (auto i : range(z.size())) { z.data()[i] = x.data()[y.data()[i]]; }
+    }
+};
+
 template <typename R>
 class im2col_2d<host_memory, traits::hw, traits::hwrs, R>
     : public traits::im2col_trait<traits::hw>
