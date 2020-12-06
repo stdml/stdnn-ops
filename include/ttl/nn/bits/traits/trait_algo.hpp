90c99de3a583c8772454d02a0e53cce026b15d82
lg <lg4869@outlook.com>
2020-12-06 01:11:58 +0000

    inline constexpr

--- include/ttl/nn/bits/traits/trait_algo.hpp
+++ include/ttl/nn/bits/traits/trait_algo.hpp
@@ -4,35 +4,59 @@
 
 namespace ttl::nn::traits
 {
-template <typename T> ttl::rank_t rank_of;
-template <> constexpr ttl::rank_t rank_of<hw> = 2;
-template <> constexpr ttl::rank_t rank_of<nchw> = 4;
-template <> constexpr ttl::rank_t rank_of<nhwc> = 4;
+template <typename T>
+ttl::rank_t rank_of;
+template <>
+inline constexpr ttl::rank_t rank_of<hw> = 2;
+template <>
+inline constexpr ttl::rank_t rank_of<nchw> = 4;
+template <>
+inline constexpr ttl::rank_t rank_of<nhwc> = 4;
 
-template <typename T> ttl::rank_t batch_position;
-template <> constexpr ttl::rank_t batch_position<nchw> = 0;
-template <> constexpr ttl::rank_t batch_position<nhwc> = 0;
+template <typename T>
+ttl::rank_t batch_position;
+template <>
+inline constexpr ttl::rank_t batch_position<nchw> = 0;
+template <>
+inline constexpr ttl::rank_t batch_position<nhwc> = 0;
 
-template <typename T> ttl::rank_t channel_position;
-template <> constexpr ttl::rank_t channel_position<chw> = 0;
-template <> constexpr ttl::rank_t channel_position<hwc> = 2;
-template <> constexpr ttl::rank_t channel_position<nchw> = 1;
-template <> constexpr ttl::rank_t channel_position<nhwc> = 3;
+template <typename T>
+ttl::rank_t channel_position;
+template <>
+inline constexpr ttl::rank_t channel_position<chw> = 0;
+template <>
+inline constexpr ttl::rank_t channel_position<hwc> = 2;
+template <>
+inline constexpr ttl::rank_t channel_position<nchw> = 1;
+template <>
+inline constexpr ttl::rank_t channel_position<nhwc> = 3;
 
-template <typename T> ttl::rank_t height_position;
-template <> constexpr ttl::rank_t height_position<chw> = 1;
-template <> constexpr ttl::rank_t height_position<hwc> = 0;
-template <> constexpr ttl::rank_t height_position<nchw> = 2;
-template <> constexpr ttl::rank_t height_position<nhwc> = 1;
+template <typename T>
+ttl::rank_t height_position;
+template <>
+inline constexpr ttl::rank_t height_position<chw> = 1;
+template <>
+inline constexpr ttl::rank_t height_position<hwc> = 0;
+template <>
+inline constexpr ttl::rank_t height_position<nchw> = 2;
+template <>
+inline constexpr ttl::rank_t height_position<nhwc> = 1;
 
-template <typename T> ttl::rank_t width_position;
-template <> constexpr ttl::rank_t width_position<chw> = 2;
-template <> constexpr ttl::rank_t width_position<hwc> = 1;
-template <> constexpr ttl::rank_t width_position<nchw> = 3;
-template <> constexpr ttl::rank_t width_position<nhwc> = 2;
+template <typename T>
+ttl::rank_t width_position;
+template <>
+inline constexpr ttl::rank_t width_position<chw> = 2;
+template <>
+inline constexpr ttl::rank_t width_position<hwc> = 1;
+template <>
+inline constexpr ttl::rank_t width_position<nchw> = 3;
+template <>
+inline constexpr ttl::rank_t width_position<nhwc> = 2;
 
-template <typename T> constexpr ttl::rank_t bias_position = channel_position<T>;
-template <> constexpr ttl::rank_t bias_position<hw> = 1;
+template <typename T>
+inline constexpr ttl::rank_t bias_position = channel_position<T>;
+template <>
+inline constexpr ttl::rank_t bias_position<hw> = 1;
 
 template <typename order, ttl::rank_t r>
 ttl::shape<4>::dimension_type batch_size(const ttl::shape<r> &x)
@@ -53,21 +77,33 @@ ttl::shape<2> image_shape(const ttl::shape<r> &x)
                          std::get<width_position<order>>(x.dims()));
 }
 
-template <typename T> ttl::rank_t filter_height_position;
-template <> constexpr ttl::rank_t filter_height_position<rscd> = 0;
-template <> constexpr ttl::rank_t filter_height_position<dcrs> = 2;
+template <typename T>
+ttl::rank_t filter_height_position;
+template <>
+inline constexpr ttl::rank_t filter_height_position<rscd> = 0;
+template <>
+inline constexpr ttl::rank_t filter_height_position<dcrs> = 2;
 
-template <typename T> ttl::rank_t filter_width_position;
-template <> constexpr ttl::rank_t filter_width_position<rscd> = 1;
-template <> constexpr ttl::rank_t filter_width_position<dcrs> = 3;
+template <typename T>
+ttl::rank_t filter_width_position;
+template <>
+inline constexpr ttl::rank_t filter_width_position<rscd> = 1;
+template <>
+inline constexpr ttl::rank_t filter_width_position<dcrs> = 3;
 
-template <typename T> ttl::rank_t filter_in_channel_position;
-template <> constexpr ttl::rank_t filter_in_channel_position<rscd> = 2;
-template <> constexpr ttl::rank_t filter_in_channel_position<dcrs> = 1;
+template <typename T>
+ttl::rank_t filter_in_channel_position;
+template <>
+inline constexpr ttl::rank_t filter_in_channel_position<rscd> = 2;
+template <>
+inline constexpr ttl::rank_t filter_in_channel_position<dcrs> = 1;
 
-template <typename T> ttl::rank_t filter_out_channel_position;
-template <> constexpr ttl::rank_t filter_out_channel_position<rscd> = 3;
-template <> constexpr ttl::rank_t filter_out_channel_position<dcrs> = 0;
+template <typename T>
+ttl::rank_t filter_out_channel_position;
+template <>
+inline constexpr ttl::rank_t filter_out_channel_position<rscd> = 3;
+template <>
+inline constexpr ttl::rank_t filter_out_channel_position<dcrs> = 0;
 
 template <typename order, ttl::rank_t r>
 ttl::shape<2> filter_shape(const ttl::shape<r> &s)
@@ -90,9 +126,11 @@ ttl::shape<4>::dimension_type filter_out_channel_size(const ttl::shape<r> &s)
 
 namespace internal
 {
-template <typename order> struct batched_image_shape_impl;
+template <typename order>
+struct batched_image_shape_impl;
 
-template <> struct batched_image_shape_impl<nhwc> {
+template <>
+struct batched_image_shape_impl<nhwc> {
     using dim_t = ttl::shape<4>::dimension_type;
     ttl::shape<4> operator()(dim_t n, const ttl::shape<2> &shp, dim_t c) const
     {
@@ -101,7 +139,8 @@ template <> struct batched_image_shape_impl<nhwc> {
     }
 };
 
-template <> struct batched_image_shape_impl<nchw> {
+template <>
+struct batched_image_shape_impl<nchw> {
     using dim_t = ttl::shape<4>::dimension_type;
     ttl::shape<4> operator()(dim_t n, const ttl::shape<2> &shp, dim_t c) const
     {
@@ -110,9 +149,11 @@ template <> struct batched_image_shape_impl<nchw> {
     }
 };
 
-template <typename order> struct conv_filter_shape_impl;
+template <typename order>
+struct conv_filter_shape_impl;
 
-template <> struct conv_filter_shape_impl<rscd> {
+template <>
+struct conv_filter_shape_impl<rscd> {
     using dim_t = ttl::shape<4>::dimension_type;
     ttl::shape<4> operator()(dim_t c, const ttl::shape<2> &shp, dim_t d) const
     {
@@ -121,7 +162,8 @@ template <> struct conv_filter_shape_impl<rscd> {
     }
 };
 
-template <> struct conv_filter_shape_impl<dcrs> {
+template <>
+struct conv_filter_shape_impl<dcrs> {
     using dim_t = ttl::shape<4>::dimension_type;
     ttl::shape<4> operator()(dim_t c, const ttl::shape<2> &shp, dim_t d) const
     {
